c0e9359a5fddfb45d764ade5120ee151d38388f7
anakrash <anakrash@vbox.42prague.com>
2025-08-17 21:11:01 +0200

    Wordpress

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,4 +1,3 @@
-#!bin/sh
 if [ ! -f "/var/www/wp-config.php" ]; then
 cat << EOF > /var/www/wp-config.php
 <?php
@@ -12,12 +11,8 @@ define('FS_METHOD','direct');
 \$table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' );}
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', 0 );
+    define( 'ABSPATH', __DIR__ . '/' );
+}
 require_once ABSPATH . 'wp-settings.php';
 EOF
 fi
