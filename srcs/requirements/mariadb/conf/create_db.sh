1018e600a18793be8d328539867f749e0bc74b3b
anakrash <anakrash@vbox.42prague.com>
2025-08-17 20:43:08 +0200

    improve

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,17 +1,15 @@
 # Init database if not done yet
 if [ ! -d "/var/lib/mysql/mysql" ]; then
         chown -R mysql:mysql /var/lib/mysql
-        mysql_install_db --user=mysql
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
         mysqld --user=mysql --bootstrap << EOF
-USE mysql;
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE ${DB_NAME};
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
 EOF
 fi
